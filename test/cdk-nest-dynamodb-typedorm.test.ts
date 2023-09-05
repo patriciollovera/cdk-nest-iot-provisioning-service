ac47cc7549d82ca8d799210c389d92ad3fe195fe
zxBAI Llovera,Patricio (IT INF) EXTERNAL <patricio.llovera.ext@boehringer-ingelheim.com>
2023-09-05 07:43:27 -0300

    Initial commit

---
+++ test/cdk-nest-dynamodb-typedorm.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkNestDynamodbTypedorm from '../lib/cdk-nest-dynamodb-typedorm-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-nest-dynamodb-typedorm-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkNestDynamodbTypedorm.CdkNestDynamodbTypedormStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
